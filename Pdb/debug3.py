77f999e2b98e1dd3a4496a796af12e33a1e808ef
KAVIARASU C <77060075+KAVIARASU0505@users.noreply.github.com>
2023-03-11 20:03:52 +0530

    Add files via upload

---
+++ Pdb/debug3.py
@@ -0,0 +1,14 @@
+def my_function(x):
+    y = x * 2
+    z = y + 3
+    return z
+result = my_function(5)
+print(result)
+def my_function1(x):
+    y = x * 2
+    z = y + 3
+    return z
+    print("End of the Program")
+    print("End of the program")
+result1 = my_function1(6)
+print(result1)
